30f381b83b893936bb569bcce4abe98efd81128d
SumiAkter2 <akteralisha36@gmail.com>
2022-03-25 21:36:23 +0600

    added fakedata and header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Exhibit Your Beauty</h1>
+            <p>Select Your Favorite Products</p>
+        </div>
+    );
+};
+
+export default Header;
